608a9712488454ca271b752692cbf60c06b4cac9
Allen_K <allenkelly278@gmail.com>
2021-05-18 08:46:51 +0100

    commit excalidraw diagram and answers

---
+++ games_app/server/db/seeds.js
@@ -0,0 +1,41 @@
+// use a database called games_hub (non-relational database)
+
+use games_hub;
+
+// drop everything
+db.dropDatabase();
+
+db.games.insertMany([
+  {
+    name: "Love Letter",
+    playingTime: 20,
+    players: {
+      min: 2,
+      max: 4
+    }
+  },
+  {
+    name: "Catan",
+    playingTime: 90,
+    players: {
+      min: 3,
+      max: 4
+    }
+  },
+  {
+    name: "Exploding Kittens",
+    playingTime: 15,
+    players: {
+      min: 2,
+      max: 5
+    }
+  },
+  {
+    name: "Resistance",
+    playingTime: 30,
+    players: {
+      min: 5,
+      max: 10
+    }
+  }
+]);
